76adfed1a8749f828cd2f14e99498d4d81ba4a10
EmilyZhang123 <zhangbinjin_style@163.com>
2017-10-30 10:58:44 +0800

    reverse Integer

--- reverseInteger.js
+++ reverseInteger.js
@@ -2,21 +2,7 @@
  * Created by emily on 2017/7/11.
  */
 //反向一个数字
-// var reverse = function(x) {
-//     if (x>0){
-//         a=x.toString().split("").reverse().join("");
-//         // return(parseInt(a));
-//         console.log(parseInt(a));
-//     }
-//    else {
-//         a= (-x).toString().split("").reverse().join("");
-//         // return(parseInt(-a));
-//         console.log(parseInt(-a));
-//
-//     }
-//
-// };
-// reverse(-123);
+
 var reverse = function(x) {
     var res = 0;
     while(x){
@@ -26,5 +12,8 @@ var reverse = function(x) {
     if(res> Math.pow(2,31) || -res>Math.pow(2,31)){
        res = 0;
     }
-    return res;
-};  
+    // return res;
+    console.log(res);
+};
+
+reverse(-123);
