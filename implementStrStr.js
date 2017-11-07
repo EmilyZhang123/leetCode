213f8b79ce48805b79b82b430629651765ddc0ae
EmilyZhang123 <zhangbinjin_style@163.com>
2017-11-07 16:24:34 +0800

    Implement strStr()

---
+++ implementStrStr.js
@@ -0,0 +1,11 @@
+/* Created by emily on 2017/11/7. */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ * Implement strStr().
+ Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
