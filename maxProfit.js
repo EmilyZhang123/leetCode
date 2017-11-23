11127959a1b91e621ba78e583337559d02b36ef2
EmilyZhang123 <zhangbinjin_style@163.com>
2017-11-23 17:34:50 +0800

    121/122. Best Time to Buy and Sell Stock

--- maxProfit.js
+++ maxProfit.js
@@ -16,6 +16,22 @@ var maxProfit = function(prices) {
             min =prices[i];
         }
     }
-    return maxProfit
+    return maxProfit;
 };
-console.log(maxProfit([7,1,5,3,6,4]));
+// console.log(maxProfit([7,1,5,3,6,4]));
+// Design an algorithm to find the maximum profit.
+// You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times).
+// However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+var maxProfit1 = function(prices) {
+    var min=prices[0];
+    var maxProfit1=0;
+    for (var j=1;j<prices.length;j++){
+        if (prices[j]>min) {
+            maxProfit1=prices[j]-min+maxProfit1;
+        }
+        min=prices[j];
+    }
+    return maxProfit1;
+
+};
+console.log(maxProfit1([7,1,5,3,6,4]));
