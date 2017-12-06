c2ed9927b1c0124204e0c8f3404b232b740e5177
EmilyZhang123 <zhangbinjin_style@163.com>
2017-12-06 14:56:47 +0800

    605. Can Place Flowers

--- canPlaceFlowers.js
+++ canPlaceFlowers.js
@@ -5,5 +5,18 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
+    if (n==0) return true;
+    for (var i=0;i<flowerbed.length;i++){
+        if (flowerbed[i]==0){
+            var pre =(i==0?0:flowerbed[i-1]);
+            var next=(i==flowerbed.length-1?0:flowerbed[i+1])
+            if (pre+next==0){
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+    }
+    return n<=0;
 
 };
+console.log(canPlaceFlowers([1,0,0,0,1],2));
