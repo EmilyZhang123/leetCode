ea164b13d50422637a7be65e64f6a56aded7a11c
EmilyZhang123 <zhangbinjin_style@163.com>
2017-12-07 15:05:48 +0800

    189. Rotate Array modify nums in-place instead.

--- rotateArray.js
+++ rotateArray.js
@@ -5,15 +5,25 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    if(nums == null || nums.length == 0 || k % nums.length == 0) return;
-    var left=nums.splice(0,nums.length-k).reverse();
-    console.log(nums);
-    console.log('1');
-    var right=nums.reverse();
-    console.log(nums);
-    console.log('2');
-    nums=left.concat(right).reverse();
-    console.log(nums);
-    console.log('3');
+    if(nums == null || nums.length == 0 || k % nums.length == 0)
+        return;
+    var turns = k % nums.length;
+    var middle = nums.length - turns;
+
+    reverse_diy(nums, 0, middle-1); // reverse left part
+    reverse_diy(nums, middle, nums.length-1); // reverse right part
+    reverse_diy(nums, 0, nums.length-1); // reverse whole part
 };
 console.log(rotate([1,2],1));
+
+function reverse_diy(arr,s,e) {
+    while(s < e)
+    {
+        var temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+
+        s++;
+        e--;
+    }
+}
