0334ad83762461ec2150c2c7f09406b8de018159
EmilyZhang123 <zhangbinjin_style@163.com>
2017-12-28 18:36:56 +0800

    undone 448

--- findDisappearedNumbers.js
+++ findDisappearedNumbers.js
@@ -4,5 +4,12 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-
+    nums.sort(function (a, b) { return a-b });
+    var disappeared=[];
+    for (var i=0;i<nums.length;i++){
+        if(nums[i+1]-nums[i]>1){
+            // disappeared.concat([])
+        }
+    }
 };
+console.log(findDisappearedNumbers([2,5,1,6,4]));
